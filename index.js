5dcd48a3f03a334c4181199ab0fe24d831d4578e
Jasmine Duruji <jasminedrj@gmail.com>
2021-10-02 23:35:24 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+/*const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+*/
+
+function countDown() {
+  for (let countDown = 0; countDown < 11; countDown++) {
+    console.log(countDown);
+  }
+}
+const newArr = [];
+function writeCards(names, event) {
+  for (let i = 0; i < names.length; i++) {
+    newArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    debugger;
+  }
+  return newArr;
+}
